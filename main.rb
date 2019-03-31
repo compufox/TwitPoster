1f20c949b81099ae571c105090d1db1f3a4fd8ae
Zac <thezacattacks@gmail.com>
2019-03-31 14:32:07 -0400

    fixed issue with loading yaml config
    added post filtering
    added checking post privacy
    fixed infinite loop bug
    fixed local variable issues

--- main.rb
+++ main.rb
@@ -2,8 +2,15 @@ require 'yaml'
 require 'moostodon'
 require 'twitter'
 
-app_conf = YAML.load(ARGV.first || 'config.yml')
-last_post = { masto: '', twit: '' }
+Levels = ['public', 'unlisted', 'private', 'direct']
+filter = /[^\w\W]/
+
+app_conf = YAML.load(File.read(ARGV.first || 'config.yml'))
+filter = /(#{app_conf[:filter].join('|')})/ if not app_conf[:filter].nil?
+level  = Levels.index(app_conf[:privacy_level]) || 0
+privacy = /#{Levels[0..level].join('|')}/
+
+$last_post = { masto: '', twit: '' }
 
 twit_client = Twitter::REST::Client.new do |config|
   config.consumer_key = app_conf[:twitter_consumer_key]
@@ -22,16 +29,11 @@ rest = Mastodon::REST::Client.new(bearer_token: mastodon_token,
 mastodon_user = rest.verify_credentials.acct
 
 def too_long? post
-  content.length > 270
+  post.length > 270
 end
 
 def should_thread? post
-  last_post[:twit] if last_post[:masto] == post.in_reply_to_id
-end
-
-def make_post(content, options = {})
-  twit_client.update(content,
-                     in_reply_to_status_id: options[:reply_id])
+  $last_post[:twit] if $last_post[:masto] == post.in_reply_to_id
 end
 
 def trim_post content
@@ -40,9 +42,8 @@ def trim_post content
   words = content.split
 
   # we break before 280 just in case we go over
-  while too_long? line
-    line += " #{words[counter]}"
-    counter += 1
+  while not words.empty? and not too_long?(line)
+    line += " #{words.shift}"
   end
 
   return line.strip, words.join(' ')
@@ -52,9 +53,9 @@ end
 Masto.user do |post|
   next unless post.kind_of? Mastodon::Status
   next unless post.account.acct == mastodon_user
-  next unless post.visibility =~ /public|unlisted/
+  next unless post.visibility =~ privacy
+  next if not post.mentions.size.zero?
 
-  tweet = nil
   content = post.content
               .gsub(/<\/p><p>/, "\n")
               .gsub(/<("[^"]*"|'[^']*'|[^'">])*>/, '')
@@ -62,20 +63,23 @@ Masto.user do |post|
               .gsub('&lt;', '<')
               .gsub('&apos;', '\'')
 
+  next if content =~ filter
+  
   content = "cw: #{post.spoiler_text}
 
-  #{content}"
+  #{content}" if not post.spoiler_text.empty?
+
+  $last_post[:twit] = should_thread?(post)
   
   loop do
     trimmed, content = trim_post content
-
-    last_post[:twit] = should_thread?(post)
     
-    tweet = make_post(trimmed, reply_id: last_post[:twit])
+    tweet = twit_client.update(trimmed,
+                               in_reply_to_status_id: $last_post[:twit])
       
-    last_post[:masto] = post.id
-    last_post[:twit] = tweet.id
-
+    $last_post[:masto] = post.id
+    $last_post[:twit] = tweet.id
+    
     break if content.empty?
   end
 end
